b01626d43118e7c60b3373f45448bf2714a83f76
Anna <matviychuk.anna324@gmail.com>
2025-08-30 10:42:37 +0300

    Restore SearchBar and CategoryFilter components in Home page; enhance CategoryFilter to dispatch getNews with selected category and current query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>World News</h1>
-      {/* <SearchBar />
-      <CategoryFilter /> */}
+      <SearchBar />
+      <CategoryFilter />
       <NewsList />
     </div>
   );
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,15 +1,22 @@
 "use client";
-import { useDispatch } from 'react-redux';
-import { setCategory } from '../store/newsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCategory, getNews } from '../store/newsSlice';
 
 export default function CategoryFilter() {
   const dispatch = useDispatch();
   const categories = ['general', 'business', 'technology', 'sports', 'health'];
+  const query = useSelector((state) => state.news.query);
 
   return (
     <div>
       {categories.map((cat) => (
-        <button key={cat} onClick={() => dispatch(setCategory(cat))}>
+        <button
+          key={cat}
+          onClick={() => {
+            dispatch(setCategory(cat));
+            dispatch(getNews({ category: cat, query }));
+          }}
+        >
           {cat}
         </button>
       ))}
